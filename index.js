040919372f1816edecca920f1b00d43a55c8261d
rish4883 <rishabh4883@gmail.com>
2023-09-29 15:49:58 +0530

    build

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-app.use(express.static('dist'))
+
 
 let notes = [
   {
